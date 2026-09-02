051f3c3d4017c4e0fdca6374051737a97477b6d5
Hasyim <hasyimagiel@gmail.com>
2026-09-02 12:05:32 +0700

    Remove duplicate TypedDict import in state

--- chatbot/graph/state.py
+++ chatbot/graph/state.py
@@ -1,5 +1,5 @@
 import operator
-from typing import TypedDict, List, Literal, Annotated
+from typing import List, Literal, Annotated
 from typing_extensions import TypedDict
 from langchain_core.messages import BaseMessage
 from langgraph.graph.message import add_messages
